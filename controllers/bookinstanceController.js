e259ca338d122b4347ea436abad18d0d56f9d38b
brajpatel <brajeshpatel0002@gmail.com>
2023-08-03 15:49:02 +0100

    Create way to update book instances

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -39,7 +39,7 @@ exports.bookinstance_create_get = asyncHandler(async (req, res, next) => {
   const allBooks = await Book.find({}, "title").exec();
 
   res.render("bookinstance_form", {
-    title: "Create BookInstance",
+    title: "Create Book Instance",
     book_list: allBooks,
   });
 });
@@ -117,10 +117,73 @@ exports.bookinstance_delete_post = asyncHandler(async (req, res, next) => {
 
 // Display BookInstance update form on GET.
 exports.bookinstance_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance update GET");
+    // Get book instance for form
+    const [bookInstance, allBooks] = await Promise.all([
+      BookInstance.findById(req.params.id).populate("book").exec(),
+      Book.find()
+    ])
+
+    if (bookInstance === null) {
+      // No results.
+      const err = new Error("Book copy not found");
+      err.status = 404;
+      return next(err);
+    }
+
+    res.render("bookinstance_form", {
+      title: "Update Book Instance:",
+      book_list: allBooks,
+      selected_book: bookInstance.book_id,
+      bookinstance: bookInstance
+    });
 });
 
 // Handle bookinstance update on POST.
-exports.bookinstance_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: BookInstance update POST");
-});
+exports.bookinstance_update_post = [
+  // Validate and sanitize fields.
+  body("book", "Book must be specified").trim().isLength({ min: 1 }).escape(),
+  body("imprint", "Imprint must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+  body("due_back", "Invalid date")
+    .optional({ values: "falsy" })
+    .isISO8601()
+    .toDate(),
+
+  // Process request after validation and sanitization.
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a BookInstance object with escaped and trimmed data.
+    const bookInstance = new BookInstance({
+      book: req.body.book,
+      imprint: req.body.imprint,
+      status: req.body.status,
+      due_back: req.body.due_back,
+      _id: req.params.id
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors.
+      // Render form again with sanitized values and error messages.
+      const allBooks = await Book.find({}, "title").exec();
+
+      res.render("bookinstance_form", {
+        title: "Create BookInstance",
+        book_list: allBooks,
+        selected_book: bookInstance.book._id,
+        errors: errors.array(),
+        bookinstance: bookInstance,
+      });
+      return;
+    } else {
+      // Data from form is valid. Update the record.
+      const thebookinstance = await BookInstance.findByIdAndUpdate(req.params.id, bookInstance, {});
+      // Redirect to book instance detail page.
+      res.redirect(thebookinstance.url);
+    }
+  }),
+]
